c73a8f777a46b53da06a1e126c2464f3a1e8ef8d
Manel Gasmi <manel.gasmi02@gmail.com>
2025-02-18 15:23:35 +0100

    complete the bar chart

--- src/components/bar-chart/BarChartComponent.jsx
+++ src/components/bar-chart/BarChartComponent.jsx
@@ -1,41 +1,67 @@
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
-
-const BarChartComponent = ({activities}) => {
-  console.log(activities);
+const BarChartComponent = ({ activities }) => {
   const transformedData = activities.sessions.map((item, i) => ({
     ...item,
     index: i + 1,
     kg: item.kilogram, // Rename "kilogram" to "kg"
-    Kcal: item.calories // Rename "calories" to "Kcal"
+    Kcal: item.calories, // Rename "calories" to "Kcal"
   }));
-  
+  const customiseLegendText = (value, entry) => {
+    const { color } = entry;
+    if(value === 'kg') {
+      return <span style={{ color }}>Poids ({value})</span>;
+    } else {
+      return <span style={{ color }}>Calories brûlées ({value})</span>;
+    }
+  };
 
-    return (
-        <BarChart
-          width={1000}
-          height={300}
-          barSize={10}
-          data={transformedData}
-          
-          margin={{
-            top: 20,
-            right: 30,
-            left: 20,
-            bottom: 10,
-          }}
-        >
-          <XAxis dataKey="index" tick={{ dy: 20 }} />
-          <YAxis yAxisId="left" orientation="left" stroke="#282D30" />
-          <YAxis yAxisId="right" orientation="right" stroke="#E60000" />
-          <Tooltip contentStyle={{ backgroundColor: "#E60000", color: 'white'}} 
-          itemStyle={{ color: "#FFFFFF" }} 
-          labelStyle={{ display: 'none' }} 
-           />
-          
-          <Bar yAxisId="left" dataKey="kg" fill="#282D30" radius={[10, 10, 0, 0]}/>
-          <Bar yAxisId="right" dataKey="Kcal" fill="#E60000" radius={[10, 10, 0, 0]}/>
-        </BarChart>
-    );
-}
+  return (
+    <ResponsiveContainer width="100%" height={300} >
+      <h2 className="ml-10 text-base">Activité quotidienne</h2>
+      <BarChart
+        height={300}
+        barSize={10}
+        data={transformedData}
+        margin={{
+          top: -25,
+          bottom: 10,
+        }}
+      >
+        <Legend iconType="circle" iconSize="8" formatter={customiseLegendText} verticalAlign="top" align="right" height={100} />
+        <XAxis dataKey="index" tick={{ dy: 20 }} />
+        <YAxis yAxisId="left" orientation="left" stroke="#282D30" />
+        <YAxis yAxisId="right" orientation="right" stroke="#E60000" />
+        <Tooltip
+          contentStyle={{ backgroundColor: "#E60000", color: "white" }}
+          itemStyle={{ color: "#FFFFFF" }}
+          labelStyle={{ display: "none" }}
+        />
+
+        <Bar
+          yAxisId="left"
+          dataKey="kg"
+          fill="#282D30"
+          radius={[10, 10, 0, 0]}
+        />
+        <Bar
+          yAxisId="right"
+          dataKey="Kcal"
+          fill="#E60000"
+          radius={[10, 10, 0, 0]}
+        />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
 export default BarChartComponent;
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -6,7 +6,7 @@ import BarChartComponent from "../../components/bar-chart/BarChartComponent";
 const Dashboard = () => {
   const userId = 18;
   const [user, setUser] = useState(null);
-  const [userActivities, setUserActivities] = useState (null);
+  const [userActivities, setUserActivities] = useState(null);
   //const [loading, setLoading] = useState(true);
   //const [error, setError] = useState(null);
 
@@ -19,26 +19,29 @@ const Dashboard = () => {
       const userActivityData = await userService.fetchUserActivity(userId);
       setUserActivities(userActivityData);
       console.log(userActivityData);
-      
-    }
+    };
 
     getUserDetails();
     getUserActivity();
-  }
-  , []);
-
+  }, []);
 
   if (user) {
     return (
-      <main className="w-full m-[55px] mb-auto ml-[100px] max-w-[1126px]">
+      <main className="w-full m-[50px] mb-auto  max-w-[1126px]">
         <h1 className="text-5xl font-bold mb-8">
           Bonjour <span className="text-red-500">{user.userInfos.firstName}</span>
         </h1>
         <p className="congratulation">
           Félicitation ! Vous avez explosé vos objectifs hier 👏
         </p>
-        <div>
-          <BarChartComponent activities={userActivities}/>
+        <div className="flex mt-10">
+          <div className="w-3/4">
+            <div className="bg-gray-50 pb-10 pt-4" >
+              <BarChartComponent activities={userActivities} />
+            </div>
+            <div></div>
+          </div>
+          <aside className="w-1/4"></aside>
         </div>
       </main>
     );
@@ -50,9 +53,5 @@ const Dashboard = () => {
     );
   }
 };
-
-
-
-
 
 export default Dashboard;
